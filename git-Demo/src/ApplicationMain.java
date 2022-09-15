98e01144259e5f339ca2147e7c730b757b32409f
RajveerSaini02 <79181225+RajveerSaini02@users.noreply.github.com>
2022-09-15 10:54:01 -0700

    Update ApplicationMain.java
    added a titile

--- git-Demo/src/ApplicationMain.java
+++ git-Demo/src/ApplicationMain.java
@@ -11,7 +11,11 @@ public class ApplicationMain {
 				"Eggs",
 				"Milk"
 		};
-		
+			System.out.println("My Shopping List");
+             System.out.println();
+             for (String item: shopList) {
+			System.out.println(item);
+		}	
 		for (String item: shopList) {
 			System.out.println(item);
 		}	
